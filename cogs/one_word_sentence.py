97b498216525d18dbe05035ff07845416721486b
Mumatae <pianister123@gmail.com>
2020-08-21 01:42:16 -0400

    Initial commit

---
+++ cogs/one_word_sentence.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+import json
+
+
+class OneWordSentence(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.channel_id = 728722955013718037
+
+        with open('./cogs/one_word_sentence.json') as f:
+            data = json.load(f)
+
+        self.data = data
+        self.last_author_id = data['last_author_id']
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+
+        if message.channel.id == self.channel_id:
+            if not message.content.isalpha() or str(message.author.id) == self.last_author_id:
+                await message.delete()
+            else:
+                self.last_author_id = self.data['last_author_id'] = str(message.author.id)
+
+                with open('./cogs/one_word_sentence.json', 'w') as f:
+                    json.dump(self.data, f)
+
+
+def setup(bot):
+    bot.add_cog(OneWordSentence(bot))
